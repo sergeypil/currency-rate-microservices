754f7139a6317132005820b2585d60a59390b8c7
sergeypil <serejas_mail@mail.ru>
2022-06-27 17:44:36 +0600

    Initial commit

---
+++ exchange-rate/src/test/java/net/serg/exchangerate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.serg.exchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
